5e07cb4eecb438e0547668cc301179139e3ed116
Romain niam0r <niamor@mm.st>
2018-02-14 20:49:22 +0000

    videoList and videoListItem done

--- src/components/videoListItem.js
+++ src/components/videoListItem.js
@@ -1,8 +1,20 @@
 import React from 'react';
 
-const VideoListItem = () => {
+const VideoListItem = ({ video }) => {
   return (
+    <li className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img src={video.snippet.thumbnails.default.url} alt="" className="media-object"/>
+        </div>
 
+        <div className="media-body">
+          <div className="media-heading">
+            {video.snippet.title}
+          </div>
+        </div>
+      </div>
+    </li>
   )
 };
 
