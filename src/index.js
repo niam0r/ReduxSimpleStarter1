5972aa13d9e51897c8fb8d44db2e11f084d622d2
Romain niam0r <niamor@mm.st>
2018-02-19 15:57:14 +0000

    reverted to previous

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import _ from 'lodash';
+import _ from 'lodach';
 import React from 'react';
 import ReactDom from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -32,11 +32,9 @@ class App extends React.Component {
   }
 
   render() {
-    const videoSearch = _.debounce ((term) => { this.videoSearch(term) }, 300);
-
     return (
       <div>
-        <SearchBar onSearchTermChange={videoSearch} />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={selectedVideo => this.setState({selectedVideo})}
